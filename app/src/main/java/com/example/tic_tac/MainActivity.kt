dd500ebd02dc6535f9703cd45a497ed06778c988
Neel Rai <neelrai906@gmail.com>
2020-09-24 13:44:53 +0530

    initilization and button enabled

--- app/src/main/java/com/example/tic_tac/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac/MainActivity.kt
@@ -7,6 +7,11 @@ import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var PLAYER=true
+    var TURN_COUNT=0
+
+    var boardStatus=Array(3){IntArray(3)}
     lateinit var board: Array<Array<Button>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,45 +27,65 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 button.setOnClickListener(this)
             }
         }
+        initializeBoardStatus()
         resetBtn.setOnClickListener{
+            TURN_COUNT=0
+            PLAYER=true
+            initializeBoardStatus()
+        }
+    }
 
+    private fun initializeBoardStatus() {
+        for (i in 0..2){
+            for (j in 0..2){
+                boardStatus[0][0]=-1
+                board[0][0].isEnabled=true
+                board[0][0].text=""
+            }
         }
     }
 
     override fun onClick(view: View) {
         when(view.id){
             R.id.button1->{
-
-            }
-            R.id.button1->{
-
+                updateValue(row=0,col=0,player=PLAYER)
             }
             R.id.button2->{
-
+                updateValue(row=0,col=1,player=PLAYER)
             }
             R.id.button3->{
+                updateValue(row=0,col=2,player=PLAYER)
 
             }
             R.id.button4->{
-
+                updateValue(row=1,col=0,player=PLAYER)
             }
             R.id.button5->{
-
+                updateValue(row=1,col=1,player=PLAYER)
             }
             R.id.button6->{
+                updateValue(row=1,col=2,player=PLAYER)
 
             }
             R.id.button7->{
-
+                updateValue(row=2,col=0,player=PLAYER)
             }
             R.id.button8->{
-
+                updateValue(row=2,col=1,player=PLAYER)
             }
             R.id.button9->{
-
+                updateValue(row=2,col=2,player=PLAYER)
             }
 
 
         }
+    }private fun updateValue(row:Int,col:Int,player:Boolean){
+        val text=if(player)"X" else "O"
+        val value=if(player)1 else 0
+        board[row][col].apply {
+            isEnabled=false
+            setText(text)
+        }
+        boardStatus[row][col]=value
     }
 }
